ad308d90c47ce7a20b07c3feec52e18e3c900f9c
Sellin <you@example.com>
2021-05-25 16:15:26 +0200

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './views/about/about.component';
+import { HomeComponent } from './views/home/home.component';
+import { PortfolioComponent } from './views/portfolio/portfolio.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EducationComponent } from './components/content/education/education.component';
+import { SkillsComponent } from './components/content/skills/skills.component';
+import { ExperienceComponent } from './components/content/experience/experience.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent,
+    EducationComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
